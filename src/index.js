f87aa7f0f393f9201ed457ceb5c79779f3ce834b
Ana Alice <ana@ana.ana>
2019-01-29 22:29:27 -0200

    Getting location and changing the display

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './components/SeasonDisplay';
+import Loader from './components/Loader';
+
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
+    
+    componentDidMount() {
+        // console.log('My component was rendered to the screen');
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // the only way to update state is using setState
+                this.setState({ lat: position.coords.latitude})
+
+                // NEVER do that:
+                // this.state.lat = position.coords.latitude
+
+            }, //success
+            (err) => {
+                this.setState({ errorMessage: err.message })
+            } //failure
+        );
+    }
+
+    componentDidUpdate() {
+        // console.log('My component was just update - it rerendered!');
+    }
+
+    // React says we have to define render!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        } 
+
+        else if (!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />;
+        } 
+
+        return <Loader message="Please accept location request" />
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
